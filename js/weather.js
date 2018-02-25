6a4afd1cd392a9e894a8f40460f07a0f6b2b12b2
Sagar <sagarthomas27@hotmail.com>
2018-02-25 09:21:33 -0500

    Added new style.css and animation

--- js/weather.js
+++ js/weather.js
@@ -190,6 +190,61 @@ $(document).ready(function (){
                     //document.getElementById("gif").innerHTML = "<img src="+ picLink + "/>";
                     document.getElementById("music").innerHTML = "<audio autoplay src="+ musicLink + "controls> ";
                     //$('#temp').load(document.URL + ' #temp');
+
+                    switch(weathercode){
+                        case 0:
+                        case 1:
+                        case 2:
+                        case 3:
+                        case 4:animation="'stormy'";break;
+                        case 5:
+                        case 6:animation="'sunny'";break;
+                        case 7:animation="'rainy'";break;
+                        case 8:
+                        case 9:animation="'snowy'";break;
+                        case 10:
+                        case 11:
+                        case 12:animation="'rainy'";break;
+                        case 13:
+                        case 14:
+                        case 15:
+                        case 16:
+                        case 17:
+                        case 18:animation="'snowy'";break;
+                        case 19:
+                        case 20:
+                        case 21:
+                        case 22:
+                        case 23:
+                        case 24:
+                        case 25:
+                        case 26:
+                        case 27:
+                        case 28:
+                        case 29:
+                        case 30:animation="'cloudy'";break;
+                        case 31:
+                        case 32:
+                        case 33:
+                        case 34:animation="'sunny'";break;
+                        case 35:animation="'rainy'";break;
+                        case 36:animation="'sunny'";break;
+                        case 37:
+                        case 38:
+                        case 39:animation="'stormy'";break;
+                        case 40:animation="'rainy'";break;
+                        case 41:
+                        case 42:
+                        case 43:animation="'snowy'";break;
+                        case 44:animation="'cloudy'";break;
+                        case 45:
+                        case 46:
+                        case 47:animation="'rainy'";break;
+                        default:
+                            animation="underdefined";
+                    }
+        
+                    document.getElementById("animations").innerHTML = "<div class="+ animation + "></div> ";
                 
 
 
